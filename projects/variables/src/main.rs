addda32eaf2fc1a9a2b37d4b7dcc6d9d24c1f95e
aboyce <adam@aboyce.uk>
2022-08-06 13:02:21 +0100

    Added variables.

---
+++ projects/variables/src/main.rs
@@ -0,0 +1,44 @@
+fn main() {
+    mutable();
+    shadowing();
+    floating();
+    numeric_operations();
+}
+
+fn mutable() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+}
+
+fn shadowing() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+}
+
+fn floating() {
+    let _x = 2.0; // f64
+
+    let _y: f32 = 3.0; // f32
+}
+
+fn numeric_operations() {
+    // addition
+    let _sum = 5 + 10;
+
+    // subtraction
+    let _difference = 95.5 - 4.3;
+
+    // multiplication
+    let _product = 4 * 30;
+
+    // division
+    let _quotient = 56.7 / 32.2;
+    let _floored = 2 / 3; // Results in 0
+
+    // remainder
+    let _remainder = 43 % 5;
+}
