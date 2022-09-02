f9f2ab3b29201f4278a9d8edb55d8f24e7416fba
aboyce <adam@aboyce.uk>
2022-09-02 13:52:19 +0100

    Added missing files.

---
+++ projects/error_handling/src/main.rs
@@ -0,0 +1,72 @@
+use std::fs::File;
+use std::io::ErrorKind;
+use std::io::{self, Read};
+
+fn main() {
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
+    };
+}
+
+fn main_with_closures() {
+    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+}
+
+// how to propagate the error to the calling function
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+// how to propagate the error to the calling function with the question mark syntax (shortcut)
+
+// There is a difference between what the match expression from above does and what the ? operator does:
+// error values that have the ? operator called on them go through the from function,
+// defined in the From trait in the standard library, which is used to convert values
+// from one type into another. When the ? operator calls the from function,
+// the error type received is converted into the error type defined in the return type
+// of the current function
+fn read_username_from_file_with_question_mark_syntax() -> Result<String, io::Error> {
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
+
+fn read_username_from_file_with_question_mark_syntax_and_chaining() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
